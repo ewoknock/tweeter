a6687620e5379debc6e1343424f9d1a310f9396d
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-03-04 12:28:07 -0800

    refactors tweet_presenter to be cleaner

--- app/presenters/tweet_presenter.rb
+++ app/presenters/tweet_presenter.rb
@@ -3,12 +3,16 @@ class TweetPresenter
   include Rails.application.routes.url_helpers
 
   attr_reader :tweet, :current_user
+  attr_reader :liked_tweet_path, :liked_tweet_turbo_data_method, :liked_tweet_image, :liked_tweet_class
+  attr_reader :bookmarked_tweet_path, :bookmarked_tweet_turbo_data_method, :bookmarked_tweet_image, :bookmarked_tweet_class, :bookmarked_tweet_text
 
   delegate :user, :body, :likes, :likes_count, :liked_users, to: :tweet
 
   def initialize(tweet, current_user)
     @tweet = tweet
     @current_user = current_user
+    tweet_liked_by_current_user
+    tweet_bookmarked_by_current_user
   end
 
   def created_at
@@ -19,83 +23,33 @@ class TweetPresenter
     end
   end
 
-  def liked_tweet_path
-    if tweet_liked_by_current_user?
-      tweet_like_path(tweet, current_user.likes.find_by(tweet: tweet))
+  def tweet_liked_by_current_user
+    if tweet.liked_users.include?(current_user)
+      @liked_tweet_path = tweet_like_path(tweet, current_user.likes.find_by(tweet: tweet))
+      @liked_tweet_turbo_data_method = "delete"
+      @liked_tweet_image = "red-heart-icon.svg"
+      @liked_tweet_class = "action-element likes liked"
     else
-      tweet_likes_path(tweet)
+      @liked_tweet_path = tweet_likes_path(tweet)
+      @liked_tweet_turbo_data_method = "post"
+      @liked_tweet_image = "heart-outline.svg"
+      @liked_tweet_class = "action-element likes"
     end
   end
 
-  def liked_tweet_turbo_data_method
-    if tweet_liked_by_current_user?
-      "delete"
+  def tweet_bookmarked_by_current_user
+    if tweet.bookmarked_users.include?(current_user)
+      @bookmarked_tweet_path = tweet_bookmark_path(tweet, current_user.bookmarks.find_by(tweet: tweet))
+      @bookmarked_tweet_turbo_data_method = "delete"
+      @bookmarked_tweet_image = "bookmark-filled.svg"
+      @bookmarked_tweet_class = "action-element bookmarked"
+      @bookmarked_tweet_text = "Bookmarked"
     else
-      "post"
+      @bookmarked_tweet_path = tweet_bookmarks_path(tweet)
+      @bookmarked_tweet_turbo_data_method = "post"
+      @bookmarked_tweet_image = "bookmark-outline.svg"
+      @bookmarked_tweet_class = "action-element normal bookmarks"
+      @bookmarked_tweet_text = "Bookmark"
     end
-  end
-
-  def liked_tweet_image
-    if tweet_liked_by_current_user?
-      "red-heart-icon.svg"
-    else
-      "heart-outline.svg"
-    end
-  end
-
-  def liked_tweet_class
-    if tweet_liked_by_current_user?
-      "action-element likes liked"
-    else
-      "action-element"
-    end
-  end
-
-  def tweet_liked_by_current_user?
-    @tweet_liked_by_current_user ||= tweet.liked_users.include?(current_user)
-  end
-
-  def bookmarked_tweet_path
-    if tweet_bookmarked_by_current_user?
-      tweet_bookmark_path(tweet, current_user.bookmarks.find_by(tweet: tweet))
-    else
-      tweet_bookmarks_path(tweet)
-    end
-  end
-
-  def bookmarked_tweet_turbo_data_method
-    if tweet_bookmarked_by_current_user?
-      "delete"
-    else
-      "post"
-    end
-  end
-
-  def bookmarked_tweet_image
-    if tweet_bookmarked_by_current_user?
-      "bookmark-filled.svg"
-    else
-      "bookmark-outline.svg"
-    end
-  end
-
-  def bookmarked_tweet_class
-    if tweet_bookmarked_by_current_user?
-      "action-element bookmarks bookmarked"
-    else
-      "action-element normal"
-    end
-  end
-
-  def bookmarked_tweet_text
-    if tweet_bookmarked_by_current_user?
-      "Bookmarked"
-    else
-      "Bookmark"
-    end
-  end
-
-  def tweet_bookmarked_by_current_user?
-    @tweet_bookmarked_by_current_user ||= tweet.bookmarked_users.include?(current_user)
   end
 end
